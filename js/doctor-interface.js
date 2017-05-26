535788fb421a8a09fb1c31acca107c585b14882a
Bryan Lin <bryanlin39@gmail.com>
2017-05-26 11:47:34 -0700

    add form to HTML and frontend JS to create a Doctor object with the user inputs

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,1 +1,12 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
+
+$(document).ready(function() {
+  $('#doctor-form').submit(function() {
+    event.preventDefault();
+    var inputtedName = $('#name').val();
+    var inputtedSpecialty = $('#specialty').val();
+
+    searchObject = new Doctor(inputtedName, inputtedSpecialty);
+    
+  });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -1,12 +1,12 @@
 var apiKey = require('./../.env').apiKey;
 
-function Doctor(name, specialization) {
+function Doctor(name, specialty) {
   this.name = name;
-  this.specialization = specialization;
+  this.specialty = specialty;
 }
 
-Doctor.prototype.getDoctors = function(name, specialization) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?name=' + name + '&query=' + specialization + '&location=wa-seattle&skip=0&limit=10&user_key=' + apiKey)
+Doctor.prototype.getDoctors = function(name, specialty) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?name=' + name + '&query=' + specialty + '&location=wa-seattle&skip=0&limit=10&user_key=' + apiKey)
   .then(function(result) {
     console.log(result);
   })

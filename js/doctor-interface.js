454d63ad058a51d8b703b18c292bfd2f0c58371f
Bryan Lin <bryanlin39@gmail.com>
2017-05-26 14:07:35 -0700

    fix getDoctors prototype method

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,12 +1,20 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var displayResults = function(doctors) {
+  $('#display-results').empty();
+  doctors.forEach(function(doctor) {
+    $('#display-results').append('<li><ul><li><b>Doctor: </b>' + doctor.name + '</li><li><b>Practice: </b>' + doctor.practice + '</li><li><b>Address: </b>' + doctor.address + '</li><li><b>Bio: </b>' + doctor.bio + '</li></ul></li>');
+  });
+};
+
 $(document).ready(function() {
+  var searchObject = new Doctor('testname', 'testpractice', 'testaddress', 'testbio');
   $('#doctor-form').submit(function() {
     event.preventDefault();
     var inputtedName = $('#name').val();
     var inputtedSpecialty = $('#specialty').val();
 
-    searchObject = new Doctor(inputtedName, inputtedSpecialty);
-    
+    searchObject.getDoctors(inputtedName, inputtedSpecialty, displayResults);
+    console.log(searchObject);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,15 +1,28 @@
 var apiKey = require('./../.env').apiKey;
 
-function Doctor(name, specialty) {
+function Doctor(name, practice, address, bio) {
   this.name = name;
-  this.specialty = specialty;
+  this.practice = practice;
+  this.address = address;
+  this.bio = bio;
 }
 
-Doctor.prototype.getDoctors = function(name, specialty) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?name=' + name + '&query=' + specialty + '&location=wa-seattle&skip=0&limit=10&user_key=' + apiKey)
-  .then(function(result) {
-    console.log(result);
+Doctor.prototype.getDoctors = function(name, specialty, displayResults) {
+  var doctors = [];
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?name=' + name + '&query=' + specialty + '&location=wa-seattle&skip=0&limit=5&user_key=' + apiKey)
+  .then(function(response) {
+    for(var i = 0; i < 5; i++) {
+      var doctorName = response.data[i].profile.first_name + ' ' + response.data[i].profile.last_name + ' ' + response.data[i].profile.title;
+      var practiceName = response.data[i].practices[0].name;
+      var practiceAddress = response.data[i].practices[0].visit_address.street + ', ' + response.data[i].practices[0].visit_address.city + ', ' + response.data[i].practices[0].visit_address.state + ' ' + response.data[i].practices[0].visit_address.zip;
+      var bio = response.data[i].profile.bio;
+      newDoctor = new Doctor(doctorName, practiceName, practiceAddress, bio);
+      doctors.push(newDoctor);
+    }
+    console.log(doctors);
+    displayResults(doctors);
   })
+
   .fail(function(error) {
     console.log('fail');
   });
